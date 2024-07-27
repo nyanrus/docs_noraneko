9f2c648f2321f88af9a22c7ef64acb6839682e5a
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-07-27 13:47:56 +0900

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -7,5 +7,22 @@ export default {
   i18n: [
     { locale: 'en-US', text: 'English' },
     { locale: "ja-JP", text: "日本語"}
-  ]
+  ],
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://github.com/nyarus" target="_blank">
+          Nyanrus
+        </a>
+        .
+      </span>
+    )
+  }
 }
